1f3194d0c1eba2d66f5fa06680bc400ff908ec67
Peter Kim <peterkim.pk1@gmail.com>
2024-03-07 20:28:42 -0500

    Live-coded challenge 3

--- for.js
+++ for.js
@@ -20,7 +20,7 @@ for (var i = 0; i < weather_options.length; i++){
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 for (var i = 0; i < words.length; i++){
-  if (words[i].length === 3){
+  if (words[i].length===3){
     console.log(words[i]);
   }
 }
